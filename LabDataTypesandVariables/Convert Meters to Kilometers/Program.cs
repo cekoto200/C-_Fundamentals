c9bab68525ed70b67074e55707fb739b0700fb7c
Tsvetan Dilkov <cekoto_200@abv.bg>
2025-05-29 19:24:15 +0300

    Lab_DataTypes

---
+++ LabDataTypesandVariables/Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
